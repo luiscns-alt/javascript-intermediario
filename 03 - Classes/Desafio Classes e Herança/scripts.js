18ec05725a462ed61333d16fc7f8434c9caa3c55
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-17 23:54:20 -0300

    desafio

---
+++ 03 - Classes/Desafio Classes e Herança/scripts.js
@@ -0,0 +1,16 @@
+class Rect {
+  constructor(b, h) {
+    this.b = b;
+    this.h = h;
+  }
+
+  area() {
+    return this.b * this.h;
+  }
+}
+
+class Square extends Rect {
+  constructor(l) {
+    super(l, l);
+  }
+}
