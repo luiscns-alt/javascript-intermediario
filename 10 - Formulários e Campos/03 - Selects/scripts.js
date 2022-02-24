07b31096bc6b4bc2e32bf12525bab52acce937f2
Luis <luiscarlosnors@gmail.com>
2022-02-24 10:27:29 -0300

    selects

---
+++ 10 - Formulários e Campos/03 - Selects/scripts.js
@@ -0,0 +1,4 @@
+document.querySelector("#my-select");
+document.querySelector("#my-select").selectedIndex;
+document.querySelector("#my-select").value;
+document.querySelector("#my-select").selected;
